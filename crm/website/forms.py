43d2f75f81e6bbb3abfd2d4579c8173851b60dc9
Subramanya <subramanyavdj@gmail.com>
2023-10-17 12:08:35 +0530

    Registration form alerts

--- crm/website/forms.py
+++ crm/website/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-class signUpForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(label = "", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
     first_name = forms.CharField(label = "",max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
     last_name = forms.CharField(label = "", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
@@ -13,7 +13,7 @@ class signUpForm(UserCreationForm):
         fields = ('username', 'first_name', 'last_name', 'email', 'password1','password1')
 
     def __init__(self, *args: Any, **kwargs: Any):
-        super(signUpForm, self).__init__(*args, **kwargs)
+        super(SignUpForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
         self.fields['username'].label = ''
@@ -28,4 +28,3 @@ class signUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
-        
